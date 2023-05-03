d6ee535b48ccb66145e8829a8fca200abc326080
YueyOare <dracoshabond@gmail.com>
2023-05-03 23:36:41 +0300

    rewrite function

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.figure import Figure
-import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.misc import derivative
 
 
 def func(x, gamma):
+    # return np.cos(x)
     # return np.exp(-x ** 2 / gamma ** 2)
+    # return np.exp(-(x - 3) ** 2 / 2)
     return (1 / gamma) * (1 / (x ** 2 + (gamma / 2) ** 2))
 
 
@@ -50,39 +52,29 @@ def display_window():
         #         return -1
         return float(entry.get())
 
-    def update_plot(params): # a, b, n, gamma, x, v, i, t, ticks
-        if params[6] == 0:
+    def update_plot(params):  # a, b, n, gamma, x, v, i, t, dt, ticks
+        a, b, n, gamma, x, v, i, t, dt, ticks = params
+        h = 1
+        if i == 0:
             ax.clear()
             ax.relim(visible_only=True)
             ax.autoscale_view(True)
-            ax.plot(params[4], params[5], color='black')
+            ax.plot(x, v, color='black')
             ax.set_title("t = 0")
             canvas.draw_idle()
-            params[6] += 1
-            root.after(params[8], update_plot, params)  # Создание цикла обновления графика каждые
-        elif params[6] <= params[7]:  # i <= t
+            root.after(ticks, update_plot, [a, b, n, gamma, x, v, i + dt, t, dt, ticks])
+        elif i <= t:  # i <= t
             ax.clear()
-            a = params[0]
-            b = params[1]
-            n = params[2]
-            gamma = params[3]
-            x = params[4]
-            v = params[5]
-            t = params[6]
-            x_1 = np.linspace(a, b, num=n, endpoint=True)
-            # v_1 = x - v * t
-            v_1 = v
+            x_1 = x + func(np.linspace(a, b, num=n, endpoint=True), gamma) * i
+            v_1 = func(np.linspace(a, b, num=n, endpoint=True), gamma)
             ax.relim(visible_only=True)
             ax.autoscale_view(True)
             ax.plot(x_1, v_1, color='black')
-            ax.set_title("t = " + str(params[6]))
+            ax.set_title("t = " + str("{:.5f}".format(i)))
             canvas.draw_idle()
-            params[0] += 1
-            params[1] += 1
-            params[4] = x_1
-            params[5] = v_1
-            params[6] += 1
-            root.after(params[8], update_plot, params)  # Создание цикла обновления графика каждые
+            if not np.array_equal(np.sort(x_1), x_1):
+                i = t
+            root.after(ticks, update_plot, [a, b, n, gamma, x_1, v_1, i + dt, t, dt, ticks])
         return
 
     def count():
@@ -93,13 +85,14 @@ def display_window():
         h = validate(entry3, 1)
         n = int(np.abs(b - a) / h)
         t = validate(entry4, 1)
-        ticks = int(validate(entry5, 1))
-        gamma = validate(entry6, 1)
+        dt = validate(entry5, 1)
+        ticks = int(validate(entry6, 1))
+        gamma = validate(entry7, 1)
         if validation:
             ax.clear()
             x = np.linspace(a, b, num=n, endpoint=True)
-            v = func(x, gamma)
-            update_plot([a + h, b + h, n, gamma, x, v, 0, t, ticks])
+            v = np.array(func(x, gamma))
+            update_plot([a + h, b + h, n, gamma, x, v, 0, t, dt, ticks])
 
     root = tk.Tk()
     root.wm_title("Распад волн")
@@ -131,27 +124,31 @@ def display_window():
     entry4 = tk.Entry(master=frame2)
     entry5 = tk.Entry(master=frame2)
     entry6 = tk.Entry(master=frame2)
+    entry7 = tk.Entry(master=frame2)
     entry1.insert(0, "-1")
     entry2.insert(0, "1")
     entry3.insert(0, "0.001")
-    entry4.insert(0, "10")
-    entry5.insert(0, "100")
-    entry6.insert(0, "1")
+    entry4.insert(0, "2")
+    entry5.insert(0, "0.001")
+    entry6.insert(0, "10")
+    entry7.insert(0, "1")
     tk.Label(text="Функция", master=frame2).grid(row=0, column=0, padx=15, sticky=tk.NSEW)
     tk.Label(text="1/gamma * 1/x^2+gamma^2/4}", master=frame2).grid(row=0, column=1, padx=15, sticky=tk.NSEW)
     tk.Label(text="a", master=frame2).grid(row=1, column=0, padx=15, sticky=tk.NSEW)
     tk.Label(text="b", master=frame2).grid(row=2, column=0, padx=15, sticky=tk.NSEW)
     tk.Label(text="h", master=frame2).grid(row=3, column=0, padx=15, sticky=tk.NSEW)
     tk.Label(text="t", master=frame2).grid(row=4, column=0, padx=15, sticky=tk.NSEW)
-    tk.Label(text="Скорость", master=frame2).grid(row=5, column=0, padx=15, sticky=tk.NSEW)
-    tk.Label(text="gamma", master=frame2).grid(row=6, column=0, padx=15, sticky=tk.NSEW)
+    tk.Label(text="dt", master=frame2).grid(row=5, column=0, padx=15, sticky=tk.NSEW)
+    tk.Label(text="Скорость", master=frame2).grid(row=6, column=0, padx=15, sticky=tk.NSEW)
+    tk.Label(text="gamma", master=frame2).grid(row=7, column=0, padx=15, sticky=tk.NSEW)
     entry1.grid(row=1, column=1, padx=15, sticky=tk.NSEW)
     entry2.grid(row=2, column=1, padx=15, sticky=tk.NSEW)
     entry3.grid(row=3, column=1, padx=15, sticky=tk.NSEW)
     entry4.grid(row=4, column=1, padx=15, sticky=tk.NSEW)
     entry5.grid(row=5, column=1, padx=15, sticky=tk.NSEW)
     entry6.grid(row=6, column=1, padx=15, sticky=tk.NSEW)
-    button1.grid(row=7, column=0, columnspan=2, pady=2)
+    entry7.grid(row=7, column=1, padx=15, sticky=tk.NSEW)
+    button1.grid(row=8, column=0, columnspan=2, pady=2)
 
     frame1.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky=tk.NSEW)
     frame2.grid(row=0, column=2, columnspan=1, padx=5, pady=5, sticky=tk.NSEW)
